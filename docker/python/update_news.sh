4c0417e391319563adaec52dbb50215de64826f9
diegoami <diego.amicabile@gmail.com>
2017-09-16 20:35:00 +0200

    Added update_news

--- docker/python/update_news.sh
+++ docker/python/update_news.sh
@@ -1,3 +1,5 @@
+git config --global user.email "diego.amicabile@gmail.com"
+git config --global user.name "Diego Amicabile"
 git clone git@github.com:diegoami/newscollection.git
 cd /app/newscollection/scrapers
 git clone git@github.com:diegoami/techcrunch-posts-scraper.git
