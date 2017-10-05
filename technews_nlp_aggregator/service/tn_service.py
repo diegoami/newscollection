740142a49967041cb8d9bb8ca2ba8f0feebcd860
Diego Amicabile <diego.amicabile@gmail.com>
2017-10-05 03:26:08 +0200

    OPtions stuff

--- technews_nlp_aggregator/service/tn_service.py
+++ technews_nlp_aggregator/service/tn_service.py
@@ -32,7 +32,7 @@ class ClassifierService(Resource):
     def after_request(response):
         response.headers.add('Access-Control-Allow-Origin', '*')
         response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
         return response
 
 
