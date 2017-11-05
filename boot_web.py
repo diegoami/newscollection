eeaa74c761af78a14aa93233048bf27b7c8c3628
Diego Amicabile <diego.amicabile@gmail.com>
2017-11-05 10:41:31 +0100

    gunicorn compatible

--- boot_web.py
+++ boot_web.py
@@ -15,6 +15,7 @@ key_config = yaml.safe_load(open(config["key_file"]))
 key_config.get("secret_key")
 app.config['SECRET_KEY'] = key_config.get("secret_key")
 #app.run(debug=True,  use_reloader=False, host='0.0.0.0',port=8080)
+app.application = Application()
 if __name__ == '__main__':
-    app.application = Application()
+
     app.run(debug=True,  use_reloader=False)
