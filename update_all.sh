978e80d1c2782269aa539744715f782fff24450b
Diego Amicabile <diego.amicabile@gmail.com>
2017-11-08 00:55:10 +0100

    no more gram generator

--- update_all.sh
+++ update_all.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-pkill gunicorn
 python scrape_site.py
 [ $? -eq 0 ] &&  python token_model_wf.py
 [ $? -eq 0 ] &&  python gram_model_wf.py
@@ -9,4 +8,12 @@ python scrape_site.py
 [ $? -eq 0 ] &&  python tfidf_matrix_wf.py
 [ $? -eq 0 ] &&  python doc2vec_model_wf.py
 [ $? -eq 0 ] &&  python similar_articles_wf.py
-
+if [ $? -eq 0 ] ; then
+     find /media/diego/QData/techarticles/models/doc2vec | xargs rm -f -r
+     find /media/diego/QData/techarticles/models/lsi | xargs rm -f -r
+     find /media/diego/QData/techarticles/models/phrases | xargs rm -f -r
+     find /media/diego/QData/techarticles/pickle | xargs rm -f -r
+     rm /media/diego/QData/techarticles/models.tgz
+     tar cvfz /media/diego/QData/techarticles/models.tgz /media/diego/QData/techarticles/models
+fi
+if [ $? -eq 0 ] ; then
