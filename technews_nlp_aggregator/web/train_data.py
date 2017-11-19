8d9768f4d8e8a4cf23de0347464190a7f1dea8d9
Diego Amicabile <diego.amicabile@gmail.com>
2017-11-19 01:36:43 +0100

    has_next

--- technews_nlp_aggregator/web/train_data.py
+++ technews_nlp_aggregator/web/train_data.py
@@ -14,9 +14,10 @@ def train_data(page_id=0):
     start, end = page_id * 100, (page_id + 1) * 100
     _.articleSimilarLoader.load_train_data_aug()
     train_dataDF = _.articleSimilarLoader.train_data
+    has_next = len(train_dataDF) > end
 
-    has_next = len(train_dataDF ) > end
     train_dataDF= train_dataDF[start:min(end, len(train_dataDF ))]
+
     train_with_titlesDF = train_dataDF.merge(articlesDF, left_on='SCO_AIN_ID_1', right_on='article_id', suffixes=('','_1') ).merge(articlesDF, left_on='SCO_AIN_ID_2', right_on='article_id',suffixes=('','_2') )
     train_data = train_with_titlesDF.to_dict('records')
-    return render_template('train_data.html', train_data =train_data , page_id=page_id)
+    return render_template('train_data.html', train_data =train_data , page_id=page_id, has_next=has_next)
