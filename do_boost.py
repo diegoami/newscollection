19c117a2747f2d91081e871acac4b5badae44ff5
Diego Amicabile <diego.amicabile@gmail.com>
2017-11-22 22:39:30 +0100

    added classifier

--- do_boost.py
+++ do_boost.py
@@ -21,6 +21,7 @@ def print_best_parameters( classifier):
 
 
 def create_regressor(train_DF, xboost_model_file):
+    print(" ============= REGRESSOR ====================== ")
   # train_df = pd.read_csv(train_file, index_col=0)
     relevant_columns = ['SCO_DAYS','SCO_D_TEXT', 'SCO_T_TEXT','SCO_D_TITLE',  'SCO_T_TITLE', 'SCO_T_SUMMARY', 'SCO_D_SUMMARY', 'SCO_T_SUMMARY_2', 'SCO_D_SUMMARY_2', 'SCO_CW_TITLE',  'SCO_CW_TEXT', 'SCO_CW_SUMMARY', 'SCO_CW_SUMMARY_2' ]
 
@@ -54,6 +55,7 @@ def create_regressor(train_DF, xboost_model_file):
 
 
 def create_classifier(train_DF, xboost_classifier_file ):
+    print(" ============= CLASSIFIER ===================== ")
   # train_df = pd.read_csv(train_file, index_col=0)
     relevant_columns = ['SCO_DAYS','SCO_D_TEXT', 'SCO_T_TEXT','SCO_D_TITLE',  'SCO_T_TITLE', 'SCO_T_SUMMARY', 'SCO_D_SUMMARY', 'SCO_T_SUMMARY_2', 'SCO_D_SUMMARY_2', 'SCO_CW_TITLE',  'SCO_CW_TEXT', 'SCO_CW_SUMMARY', 'SCO_CW_SUMMARY_2' ]
 
