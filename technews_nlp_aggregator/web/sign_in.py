c3258144c5f6f8339a7f421640015a9f895f1e78
Diego Amicabile <diego.amicabile@gmail.com>
2018-01-02 21:47:34 +0100

    Added logo

--- technews_nlp_aggregator/web/sign_in.py
+++ technews_nlp_aggregator/web/sign_in.py
@@ -1,9 +1,11 @@
 from . import app
 
 from flask import session, redirect, url_for
-@app.route('/sign_in')
-def sign_in():
-    session["signed_in"] = True
+@app.route('/sign_in/<string:key>')
+def sign_in(key):
+    _ = app.application
+    if (key == _.db_config["signin_key"]):
+        session["signed_in"] = True
     return redirect(url_for('duplicates'))
 
 @app.route('/sign_out')
