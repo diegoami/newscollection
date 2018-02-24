552bfc08e0ee9dfd7a7c61510fae012a0d432554
Ubuntu <ubuntu@ip-172-31-43-127.eu-central-1.compute.internal>
2018-02-24 08:56:50 +0000

    Tried versions

---
+++ exec_test_run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+[ $? -eq 0 ] &&  python token_model_wf.py --action create
+[ $? -eq 0 ] &&  python gram_model_wf.py
+[ $? -eq 0 ] &&  python gram_export_wf.py
+[ $? -eq 0 ] &&  python tfidf_dictionary_wf.py
+[ $? -eq 0 ] &&  python tfidf_model_wf.py
+[ $? -eq 0 ] &&  python tfidf_matrix_wf.py
+[ $? -eq 0 ] &&  python doc2vec_model_wf.py
+
+[ $? -eq 0 ] &&  python create_train_data.py
+[ $? -eq 0 ] &&  python do_boost.py
+[ $? -eq 0 ] &&  python create_test_data.py
+[ $? -eq 0 ] &&  python do_predict.py
