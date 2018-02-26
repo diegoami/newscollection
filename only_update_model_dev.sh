74b2132f5096f2c12df5e52815ce669191da3455
Ubuntu <ubuntu@ip-172-31-43-127.eu-central-1.compute.internal>
2018-02-26 08:52:00 +0000

    Last run

--- only_update_model_dev.sh
+++ only_update_model_dev.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-[ $? -eq 0 ] &&  python gram_model_wf.py
-[ $? -eq 0 ] &&  python gram_export_wf.py
-[ $? -eq 0 ] &&  python tfidf_dictionary_wf.py
-[ $? -eq 0 ] &&  python tfidf_model_wf.py
-[ $? -eq 0 ] &&  python tfidf_matrix_wf.py
+#[ $? -eq 0 ] &&  python gram_model_wf.py
+#[ $? -eq 0 ] &&  python gram_export_wf.py
+#[ $? -eq 0 ] &&  python tfidf_dictionary_wf.py
+#[ $? -eq 0 ] &&  python tfidf_model_wf.py
+#[ $? -eq 0 ] &&  python tfidf_matrix_wf.py
 [ $? -eq 0 ] &&  python doc2vec_model_wf.py
+[ $? -eq 0 ] &&  python create_train_data.py
+[ $? -eq 0 ] &&  python do_boost.py
 
 
--- update_model.sh
+++ update_model.sh
@@ -2,8 +2,7 @@
 sudo apt-get update && sudo apt-get upgrade -y
 cd /home/ubuntu/projects/newscollection/
 source /home/ubuntu/anaconda3/bin/activate tnaggregator-3
-[ $? -eq 0 ] &&  git pull origin master
-stop
+[ $? -eq 0 ] &&  git pull origin spacy2
 [ $? -eq 0 ] &&  python scrape_site.py
 [ $? -eq 0 ] &&  python scrape_urls.py
 [ $? -eq 0 ] &&  python token_model_wf.py --action append
