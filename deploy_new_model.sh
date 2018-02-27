7ea6d411291cd9caf72d86859012dfbc8a85529f
Ubuntu <ubuntu@ip-172-31-43-127.eu-central-1.compute.internal>
2018-02-27 18:00:40 +0000

    Saved current configuration

--- deploy_new_model.sh
+++ deploy_new_model.sh
@@ -4,5 +4,5 @@
 #ssh ubuntu@18.194.147.93 'find /media/diego/QData/techarticles/models/phrases -mtime +3 -type d | xargs rm -f -r'
 #ssh ubuntu@18.194.147.93 'find /media/diego/QData/techarticles/pickle | xargs rm -f -r'
 #ssh ubuntu@18.194.147.93 'rm /media/diego/QData/techarticles.tgz'
-scp /media/diego/QData/techarticles.tgz ubuntu@52.28.241.232:/media/diego/QData
+scp /media/diego/QData/techarticles.tgz ubuntu@18.196.236.228:/media/diego/QData
 #ssh ubuntu@18.194.147.93 'pushd /media/diego/QData; tar xvf techarticles.tgz; popd'
