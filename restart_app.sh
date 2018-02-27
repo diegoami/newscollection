af6287d0501936e63c60e13fca45a0ca58a97837
Ubuntu <ubuntu@ip-172-31-41-199.eu-central-1.compute.internal>
2018-02-27 18:09:07 +0000

    Modified script for restart_app

--- restart_app.sh
+++ restart_app.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
 cd /home/ubuntu/projects/newscollection/
-source activate tnaggregator-2
-nohup   gunicorn boot_web:app --timeout 120 --bind=0.0.0.0:8080 -w 1 --error-logfile=gunicorn-error.log --access-logfile=gunicorn-access.log &
+source /home/ubuntu/anaconda3/bin/activate tnaggregator-3
+git pull origin spacy2
+nohup /home/ubuntu/anaconda3/envs/tnaggregator-3/bin/gunicorn boot_web:app --timeout 120 --bind=0.0.0.0:8080 -w 1 --error-logfile=gunicorn-error.log --access-logfile=gunicorn-access.log &
