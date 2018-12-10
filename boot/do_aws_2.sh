e88e05c08de35d0b55080f6dbb8a64d5d76cff89
Diego Amicabile <diego.amicabile@gmail.com>
2018-12-10 01:29:31 +0100

    save conf do_aws_2.sh

--- boot/do_aws_2.sh
+++ boot/do_aws_2.sh
@@ -4,20 +4,15 @@ export PATH="/home/ubuntu/anaconda3/bin:$PATH"
 export PATH="/home/ubuntu/.local/bin:$PATH"
 cd /home/ubuntu/awscli/
 /home/ubuntu/.local/bin/aws ec2 start-instances --instance-ids $1
-sleep 45m
+sleep 50m
 while [ ! -f /media/diego/QData/techarticles.tgz ]
 do
   sleep 1m
 done
-sleep 1m
+while [ ! $(wc -c <"/media/diego/QData/techarticles.tgz") -ge 1200000000 ]
+do
+  sleep 3m
+done
 /home/ubuntu/.local/bin/aws ec2 stop-instances --instance-ids $1
-find /media/diego/QData/techarticles/models/doc2vec -mtime +3 -type d | xargs rm -f -r
-find /media/diego/QData/techarticles/models/lsi -mtime +3 -type d | xargs rm -f -r
-find /media/diego/QData/techarticles/models/phrases -mtime +3 -type d | xargs rm -f -r
-find /media/diego/QData/techarticles/pickle -mtime +3 | xargs rm -f -r
-pushd /media/diego/QData; tar xvf techarticles.tgz; popd
-pkill gunicorn
-cd /home/ubuntu/projects/newscollection/
-git pull origin master
-sleep 30
-/home/ubuntu/projects/newscollection/restart_app.sh
+/home/ubuntu/awscli/swap_models.sh
+
