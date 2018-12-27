8dc039734a401cb81d0314b0100f1b12a0ccac0f
diegoami <diego.amicabile@gmail.com>
2018-12-27 12:21:33 +0100

    Predict

--- do_predict.py
+++ do_predict.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     logging.info("predictions_df has {} rows".format(len(predictions_df)))
 
     test_df_res = predict(test_df, xboost_model_file, xboost_classif_file, predictions_df)
-    logging.info("Preparing to write {} predictions".format(test_df_res))
+    logging.info("Preparing to write {} predictions".format(len(test_df_res)))
     num_predictions = similarArticlesRepo.write_predictions(test_df_res, config["version"])
     logging.info("{} predictions done".format(num_predictions))
 
